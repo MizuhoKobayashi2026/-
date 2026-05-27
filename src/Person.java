af90112fe6cd40dbada1a9946345c97a146f7d68
小林瑞歩 <mizuho.kobayashi@nkaihatsu.com>
2026-05-27 22:21:38 +0900

    4回目: Personの拡張

--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,28 @@
 import java.time.LocalDate;
+import java.time.Period;
 
+import lombok.Data;
 
+@Data
 public abstract class Person {
 	protected String id;
 	protected String name;
 	protected LocalDate birthDate;
 	protected String email;
+	
+	
+	public Person(String id, String name, LocalDate birthDate, String email) {
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+		this.email = email;
+	}
+	
+	//年齢計算メソッド
+	public int getAge() {
+		return Period.between(birthDate, LocalDate.now()).getYears();
+	}
+	
+	
+	//自己紹介メソッド
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,6 @@
 
 public class Student {
-
+	protected String major;
+	protected double gpa;
+	protected String club;
 }
