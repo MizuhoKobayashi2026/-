d32500188a3c9d468d3d0433b00e838599473789
小林瑞歩 <mizuho.kobayashi@nkaihatsu.com>
2026-05-30 14:38:34 +0900

    削除練習用: テストファイルの追加

---
+++ src/TempTestFile.java
@@ -0,0 +1,14 @@
+
+public class TempTestFile {
+	public static void main(String[] args) {
+        System.out.println("このファイルは削除練習用です");
+        System.out.println("Week 6レッスン4で学習後に削除されます");
+    }
+    
+    /**
+     * 削除練習用のメソッド
+     */
+    public void temporaryMethod() {
+        System.out.println("一時的なメソッドです");
+    }
+}
